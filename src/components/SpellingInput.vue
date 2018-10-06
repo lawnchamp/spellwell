ef20a84261147e2b63a1177e3e4f5b19ace8ac37
Riley Shenk <rileyshenk@learnzillion.com>
2018-10-06 17:09:15 -0400

    just getting things started

---
+++ src/components/SpellingInput.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="text-sm hello">
+    <input v-model="newSpellingWord" placeholder="Add Spelling Word">
+    <p>new word: {{newSpellingWord}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SpellingInput",
+  data() {
+    return {
+      newSpellingWord: ""
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
