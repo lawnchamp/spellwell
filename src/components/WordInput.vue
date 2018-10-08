64958547cafe157e44f8b68798a274a320fdb774
Riley Shenk <rileyshenk@learnzillion.com>
2018-10-08 17:58:55 -0400

    able to add and display words

---
+++ src/components/WordInput.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <div>
+      <input v-model="newWord.word" placeholder="Add Word">
+    </div>
+    <div>
+      <textarea v-model="newWord.definition" placeholder="Add definition here"></textarea>
+    </div>
+    <button @click="addWord">Add Word</button>
+    {{this.$store.getters.words}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WordInput',
+  data() {
+    return {
+      newWord: {
+        word: '',
+        definition: '',
+      },
+    }
+  },
+  methods: {
+    addWord() {
+      this.$store.dispatch('addWord', this.newWord).then(() => {
+        this.newWord.word = ''
+        this.newWord.definition = ''
+      })
+    },
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/store.js
+++ src/store.js
@@ -1,10 +1,49 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
+import db from '@/../firebase.js'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
-});
+  state: {
+    error: false,
+    errorMessages: [],
+    loading: false,
+    words: [],
+  },
+  mutations: {
+    ADD_WORD(state, word) {
+      state.words.push(word)
+    },
+    LOADING(state, loading) {
+      state.loading = loading
+    },
+    LOG_ERROR(state, errorMessage) {
+      state.error = true
+      state.errorMessages.push(errorMessage)
+    },
+  },
+  actions: {
+    addWord({commit}, {word, definition}) {
+      commit('LOADING', true)
+
+      const newWord = {}
+      newWord[word] = definition
+
+      db.collection('words')
+        .doc(word)
+        .set(newWord)
+        .then(() => {
+          commit('LOADING', false)
+          commit('ADD_WORD', {word, definition})
+        })
+        .catch(function(error) {
+          commit('LOG_ERROR', `failed to add ${word}: ${error}`)
+        })
+    },
+  },
+  getters: {
+    words: state => state.words,
+    hasError: state => state.error,
+  },
+})
